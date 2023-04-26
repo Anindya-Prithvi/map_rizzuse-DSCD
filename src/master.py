ab7b0a61c57cc2c041e58ea3a62cb1c4040be244
Andy Candy <anindya20024@iiitd.ac.in>
2023-04-26 17:03:09 +0530

    lambdas working

--- src/master.py
+++ src/master.py
@@ -17,6 +17,63 @@ from map_worker import Mapper
 from reduce_worker import Reducer
 
 
+class WC:
+    def parse_and_map(self, file):
+        """Prepares input for map function"""
+        with open(file, "r") as f:
+            input_data = f.read()
+            lines = input_data.split("\n")
+            for i, line in enumerate(lines):
+                self.partition(self, self.map(self, i, line.strip()))
+
+    def map(self, key, value):
+        line_words = value.split()
+        l_k_v = [(word.lower(), 1) for word in line_words]  # wc case insensitive
+        return l_k_v
+
+    def partition(self, l_k_v):
+        from hashlib import md5
+
+        hash = md5
+        # will generate n_reduce intermediate files
+
+        for key, value in l_k_v:
+            key_int = int(hash(key.encode()).hexdigest(), 16)
+            # hash the key and mod it with n_reduce
+            # write the key-value pair to the corresponding file
+            self.file_handles[key_int % self.n_reduce][0].acquire()
+            self.file_handles[key_int % self.n_reduce][1].write(f"{key} {value}\n")
+            self.file_handles[key_int % self.n_reduce][0].release()
+
+    def parse_map_loc(self, map_loc):
+        """This function will read all assigned intermediate files of the mapper"""
+        for file in os.listdir(map_loc):
+            self.shufflesort(self, os.path.join(map_loc, file, self.node_name))
+        self.reduce(self)
+
+    # will only be called when IF received from all mappers
+    def reduce(self):
+        """function to reduce the values that belong to the same key."""
+        with open(f"{self.output_dir}/Output{self.node_name}.txt", "w") as f:
+            for key in self.hashbucket:
+                f.write(f"{key} {sum(self.hashbucket[key])}\n")
+
+    def shufflesort(self, file):
+        """function to sort the intermediate key-value pairs by key and
+        group the values that belong to the same key.
+        """
+        # check if file exists [no work for reducer]
+        if not os.path.isfile(file):
+            return
+        with open(file, "r") as f:
+            for line in f:
+                key, value = line.strip().split(" ")
+                value = int(value)
+                if key not in self.hashbucket:
+                    self.hashbucket[key] = []
+                self.hashbucket[key].append(value)  # mostly 1 since no local reduce
+
+
 class Master:
     """Master node class"""
 
@@ -141,6 +198,9 @@ class Master:
                     "IP": "[::1]",
                     "n_reduce": self.n_reduce,
                     "intermediate_storage": self.intermediate,
+                    "partition": WC.partition,
+                    "parse_and_map": WC.parse_and_map,
+                    "map": WC.map,
                 },
             )
             p.start()
@@ -155,6 +215,9 @@ class Master:
                     "IP": "[::1]",
                     "n_map": self.n_map,
                     "output_dir": self.output_data,
+                    "reduce": WC.reduce,
+                    "parse_map_loc": WC.parse_map_loc,
+                    "shufflesort": WC.shufflesort,
                 },
             )
             p.start()
--- src/reduce_worker.py
+++ src/reduce_worker.py
@@ -1,4 +1,3 @@
-import os
 from concurrent import futures
 
 import grpc
@@ -14,17 +13,20 @@ class ReduceProcessInput(messages_pb2_grpc.ReduceProcessInputServicer):
     def Receive(self, request, context):
         """This function is called by the master node to send files"""
         self.reducer.node_name = request.key
-        self.reducer.parse_map_loc(request.value)
+        self.reducer.parse_map_loc(self.reducer, request.value)
 
         return messages_pb2.Success(value="SUCCESS")
 
 
 class Reducer:
-    def __init__(self, PORT, IP, n_map, output_dir):
+    def __init__(self, PORT, IP, n_map, output_dir, parse_map_loc, reduce, shufflesort):
         # create directory to store intermediate files
         self.node_name = None
         self.hashbucket = {}
         self.output_dir = output_dir
+        self.parse_map_loc = parse_map_loc
+        self.reduce = reduce
+        self.shufflesort = shufflesort
 
         port = str(PORT)
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=50))
@@ -35,31 +37,3 @@ class Reducer:
         server.start()
         # logger.debug(f"{self.intermediate_dir} started on {IP}:{port}")
         server.wait_for_termination()
-
-    def parse_map_loc(self, map_loc):
-        """This function will read all assigned intermediate files of the mapper"""
-        for file in os.listdir(map_loc):
-            self.shufflesort(os.path.join(map_loc, file, self.node_name))
-        self.reduce()
-
-    # will only be called when IF received from all mappers
-    def reduce(self):
-        """function to reduce the values that belong to the same key."""
-        with open(f"{self.output_dir}/Output{self.node_name}.txt", "w") as f:
-            for key in self.hashbucket:
-                f.write(f"{key} {sum(self.hashbucket[key])}\n")
-
-    def shufflesort(self, file):
-        """function to sort the intermediate key-value pairs by key and
-        group the values that belong to the same key.
-        """
-        # check if file exists [no work for reducer]
-        if not os.path.isfile(file):
-            return
-        with open(file, "r") as f:
-            for line in f:
-                key, value = line.strip().split(" ")
-                value = int(value)
-                if key not in self.hashbucket:
-                    self.hashbucket[key] = []
-                self.hashbucket[key].append(value)  # mostly 1 since no local reduce
